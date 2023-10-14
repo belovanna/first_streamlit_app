1c7148b868a6f397f9bbc9e2cd30410e5d25f298
belovanna <anabelova@gmail.com>
2023-10-14 22:16:09 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,11 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
 
-
+#New Section to display fruityvice api response
+streamlit.header ('Fruityvice Fruit Advice! ')
+import requests
+fruityvice_response=requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice response.json())
 
 
 
