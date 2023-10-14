fe7b15d370a89552aa88f0359eda36493353fa02
belovanna <anabelova@gmail.com>
2023-10-14 22:29:32 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,8 +23,11 @@ streamlit.dataframe(fruits_to_show)
 #New Section to display fruityvice api response
 streamlit.header ('Fruityvice Fruit Advice! ')
 
+fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
+streamlit.write('The user entered', fruit_choice)
+
 import requests
-fruityvice_response2=requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruityvice_response2=requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # takes json and normalized
 fruityvice_normalized = pandas.json_normalize(fruityvice_response2.json())
