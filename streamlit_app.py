0a4decb365ecc6714b686dbf657a6d367e77d1b1
belovanna <anabelova@gmail.com>
2023-10-15 14:08:21 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,7 @@ try:
   # writing data into a table
   streamlit.dataframe(fruityvice_normalized)
 except URLError as e:
-streamlit.error()
+  streamlit.error()
 
 streamlit.write('The user entered', fruit_choice)
 
