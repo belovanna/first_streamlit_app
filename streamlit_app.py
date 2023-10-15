94bb644a4845ed464f2bb83e0a185c6528453da9
belovanna <anabelova@gmail.com>
2023-10-15 13:22:15 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,8 +9,6 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import snowflake.connector
-
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -29,6 +27,7 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
 streamlit.write('The user entered', fruit_choice)
 
 import requests
+
 fruityvice_response2=requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # takes json and normalized
@@ -36,6 +35,12 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response2.json())
 # writing data into a table
 streamlit.dataframe(fruityvice_normalized)
 
+import snowflake.connector
 
-
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
 
